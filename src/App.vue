853a790c4932e65aa8b570ef1b81c1d7140ed025
Ondrej Muzik <ondrej@kizum-1168.docsis.vodafone.cz>
2024-04-16 15:43:42 +0200

    Code cleanup (no functional changes)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
 
-    <div v-if="type" >
-    
+    <div v-if="type">
        <Default  :type="this.type" :index="this.selectedIndex"/>
- 
      </div>
      
      <div v-else class="container" >
@@ -14,11 +12,11 @@
         </div>
       </div>
     </div>
+
 </template>
 
 <script>
 import Default from "./components/Default.vue";
-
 
 export default {
   data() {
@@ -30,20 +28,12 @@ export default {
         { src: "/src/assets/cube.png", alt: "Cube" },
         { src: "/src/assets/fade.png", alt: "Fade" },
         { src: "/src/assets/creative.png", alt: "Fade" },
-        
         // Add more images as needed
       ],
-
-
-      
     };
-     
-
-    
   },
   components: {
     Default,
-
   },
   methods: {
     chooseType(index) {
@@ -61,74 +51,74 @@ export default {
         this.type = 'single'
       }
     },
-    
   },
 };
 </script>
 
- <style >
-
-body{
-  background-color: #0b3144!important;
-}
-
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh; /* This makes the container take up the full viewport height */
-  flex-direction: column; /* This ensures the content is centered vertically */
- }
- 
- 
- .image-grid {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  gap: 20px;
- }
- 
- .image-container {
-  position: relative;
-  display: inline-block;
-  margin: 10px;
-  
- }
- 
- .image-container img {
-  width: 100%;
-  height: auto;
-  transition: opacity 0.5s, transform 0.5s;
- }
- 
- .image-container:hover img {
-  opacity: 0.5;
-  transform: scale(1.05);
- }
- 
- .hidden-button {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  opacity: 0;
-  transition: opacity 0.5s;
-  background-color: #ffffff;
-  color: rgb(0, 0, 0);
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
- }
- 
- .image-container:hover .hidden-button {
-  opacity: 1;
- }
- 
-
- @media (max-width: 768px) {
-  .image-grid {
-     grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+<style>
+  body {
+    background-color: #0b3144 !important;
   }
- }
- </style>
+
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    /* This makes the container take up the full viewport height */
+    flex-direction: column;
+    /* This ensures the content is centered vertically */
+  }
+
+
+  .image-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    gap: 20px;
+  }
+
+  .image-container {
+    position: relative;
+    display: inline-block;
+    margin: 10px;
+
+  }
+
+  .image-container img {
+    width: 100%;
+    height: auto;
+    transition: opacity 0.5s, transform 0.5s;
+  }
+
+  .image-container:hover img {
+    opacity: 0.5;
+    transform: scale(1.05);
+  }
+
+  .hidden-button {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+    transition: opacity 0.5s;
+    background-color: #ffffff;
+    color: rgb(0, 0, 0);
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .image-container:hover .hidden-button {
+    opacity: 1;
+  }
+
+
+  @media (max-width: 768px) {
+    .image-grid {
+      grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+    }
+  }
+</style>
  
